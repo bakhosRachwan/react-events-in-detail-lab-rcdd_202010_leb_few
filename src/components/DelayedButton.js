f7c9183e80d5bc3016ec24d4a8c0455ae46afe12
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-05 11:25:36 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,13 @@
-// Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component {
+  reduce (){
+    const coord = (e) =>{
+      return [e.clientX, e.clientY]
+    }
+    return(
+      <div><button onClick={coord}></button></div>
+    )
+  }
+}
+export default DelayedButton
